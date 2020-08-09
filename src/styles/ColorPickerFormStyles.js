a129af6f3ee20a30f311b7b213f6e4ddcf5f8b52
TKSkaushalsaraswat <thekaushalsaraswat@gmail.com>
2020-08-09 23:21:02 +0630

    'add'

---
+++ src/styles/ColorPickerFormStyles.js
@@ -0,0 +1,18 @@
+const styles = {
+  picker: {
+    width: '100% !important',
+    marginTop: '1rem',
+  },
+  addColor: {
+    width: '100%',
+    // padding: '0.5rem',
+    marginTop: '3rem',
+    fontSize: '1rem',
+  },
+  colorNameInput: {
+    width: '100%',
+    height: '10px',
+  },
+};
+
+export default styles;
